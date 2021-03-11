ac9b3a08a2a8a90a5e3232dc9c1c1e0ee15bfd83
Lav Hinsu <hinsulav105@gmail.com>
2021-03-11 23:44:54 +0530

    added docker-compose and dockerfile for flask

--- chat-app/src/components/Input.js
+++ chat-app/src/components/Input.js
@@ -2,30 +2,43 @@ import React, { useState } from "react";
 
 export default function Input({ clickProps, sender, receiver }) {
   const [answerInput, setAnswerInput] = useState("");
+  const [predictiveText, setPredictiveText] = useState("");
+
+
+  async function getPredictedText(input){
+    // axios api call here
+    // set state here
+  }
 
   return (
-    <div className="Chat_compose">
-      <input
-        className="Chat_compose_input"
-        type="text"
-        placeholder="Type a message..."
-        value={answerInput}
-        onChange={e => setAnswerInput(e.target.value)}
-      />
-      <button
-        className="Chat_compose_button button"
-        onClick={e => {
-          clickProps({
-            variables: {
-              text: answerInput,
-              sender,
-              receiver
-            }
-          });
-        }}
-      >
-        submit
-      </button>
-    </div>
+    <>
+      <div></div>
+      <div className="Chat_compose">
+        <input
+          className="Chat_compose_input"
+          type="text"
+          placeholder="Type a message..."
+          value={answerInput}
+          onChange={(e) => {
+            getPredictedText(e)
+            setAnswerInput(e.target.value);
+          }}
+        />
+        <button
+          className="Chat_compose_button button"
+          onClick={(e) => {
+            clickProps({
+              variables: {
+                text: answerInput,
+                sender,
+                receiver,
+              },
+            });
+          }}
+        >
+          submit
+        </button>
+      </div>
+    </>
   );
 }
